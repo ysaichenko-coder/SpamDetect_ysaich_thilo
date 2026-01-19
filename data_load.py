8991db73198b4b1ace909ef4fc5d0133fc44ba87
YaroslavSaichenko <ysaichenko@edu.cdv.pl>
2026-01-19 17:11:03 +0100

    fine tuning, analize data are added

--- data_load.py
+++ data_load.py
@@ -13,6 +13,8 @@ class DataLoad:
         csv_path = os.path.join(path, 'spam.csv')
         data = pd.read_csv(csv_path, encoding='latin-1')
         data = data[['v1', 'v2']]
+        print(data['v1'].value_counts())
+        print(f"total rows: {len(data)}")
         X_train, X_test, Y_train, Y_test = train_test_split(data['v2'], data['v1'], test_size=0.2, random_state=3)
         feature_extraction = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
         X_train_features = feature_extraction.fit_transform(X_train)
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import joblib
 from data_load import DataLoad
@@ -13,17 +14,19 @@ csv_path = os.path.join(path, 'spam.csv')
 loader = DataLoad()
 X_train_features, X_test_features, Y_train, Y_test, feature_extraction = loader.load_data()
 print(Y_test)
-model = SVC(kernel='sigmoid', C=1.0)
-model.fit(X_train_features, Y_train)
-prediction = model.predict(X_test_features)
-accuracy = accuracy_score(Y_test, prediction)
-print('accuracy on test',accuracy*100,'%')
-joblib.dump(model,'model.joblib')
+modelSVC = SVC(kernel='sigmoid', C=1.0)
+modelLR = LogisticRegression()
+for i in [modelSVC,modelLR]:
+    i.fit(X_train_features, Y_train)
+    prediction = i.predict(X_test_features)
+    accuracy = accuracy_score(Y_test, prediction)
+    print(i,'accuracy on test',accuracy*100,'%')
+joblib.dump(modelSVC,'model.joblib')
 input = ["Hello mom, are we having dinner tonight?",
     "URGENT! You have won a FREE iPhone. Click here",
     "Call me back later",
     "Congratulations, you won 1000$ prize money",
     "Hey bro what's up"]
 input_features = feature_extraction.transform(input)
-prediction = model.predict(input_features)
+prediction = modelSVC.predict(input_features)
 print(prediction)
