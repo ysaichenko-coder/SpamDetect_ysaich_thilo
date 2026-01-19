639d0b5f4b75b4d64c233d5f2b846051aadb3f5d
Itiar <timothyhilonenko@gmail.com>
2026-01-19 19:21:00 +0100

    Minor formating and code optimisation

--- data_load.py
+++ data_load.py
@@ -1,11 +1,8 @@
-import numpy as np
 import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.svm import SVC
-from sklearn.metrics import accuracy_score
-import joblib
+
 
 class DataLoad:
     def load_data(self):
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import numpy as np
-import pandas as pd
 import os
-from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
@@ -16,17 +12,17 @@ X_train_features, X_test_features, Y_train, Y_test, feature_extraction = loader.
 print(Y_test)
 modelSVC = SVC(kernel='sigmoid', C=1.0)
 modelLR = LogisticRegression()
-for i in [modelSVC,modelLR]:
+for i in [modelSVC, modelLR]:
     i.fit(X_train_features, Y_train)
     prediction = i.predict(X_test_features)
     accuracy = accuracy_score(Y_test, prediction)
-    print(i,'accuracy on test',accuracy*100,'%')
-joblib.dump(modelSVC,'model.joblib')
+    print(i, 'accuracy on test', accuracy * 100, '%')
+joblib.dump(modelSVC, 'model.joblib')
 input = ["Hello mom, are we having dinner tonight?",
-    "URGENT! You have won a FREE iPhone. Click here",
-    "Call me back later",
-    "Congratulations, you won 1000$ prize money",
-    "Hey bro what's up"]
+         "URGENT! You have won a FREE iPhone. Click here",
+         "Call me back later",
+         "Congratulations, you won 1000$ prize money",
+         "Hey bro what's up"]
 input_features = feature_extraction.transform(input)
 prediction = modelSVC.predict(input_features)
 print(prediction)
