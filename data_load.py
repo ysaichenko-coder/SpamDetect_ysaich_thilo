b9ab274edfe17ac1399accb4cd43ec13b2e99b40
YaroslavSaichenko <ysaichenko@edu.cdv.pl>
2026-01-19 16:29:09 +0100

    apply OOP, separate code into data_load.py and main.py

---
+++ data_load.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+import joblib
+
+class DataLoad:
+    def load_data(self):
+        path = './data'
+        csv_path = os.path.join(path, 'spam.csv')
+        data = pd.read_csv(csv_path, encoding='latin-1')
+        data = data[['v1', 'v2']]
+        X_train, X_test, Y_train, Y_test = train_test_split(data['v2'], data['v1'], test_size=0.2, random_state=3)
+        feature_extraction = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
+        X_train_features = feature_extraction.fit_transform(X_train)
+        X_test_features = feature_extraction.transform(X_test)
+        return X_train_features, X_test_features, Y_train, Y_test, feature_extraction
--- main.py
+++ main.py
@@ -6,16 +6,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import joblib
+from data_load import DataLoad
 
 path = './data'
 csv_path = os.path.join(path, 'spam.csv')
-data = pd.read_csv(csv_path,encoding='latin-1')
-data = data[['v1', 'v2']]
-X_train, X_test, Y_train, Y_test = train_test_split(data['v2'],data['v1'], test_size=0.2, random_state=3)
-feature_extraction = TfidfVectorizer(min_df=1, stop_words='english',lowercase=True)
-X_train_features = feature_extraction.fit_transform(X_train)
-X_test_features = feature_extraction.transform(X_test)
-print(data['v1'])
+loader = DataLoad()
+X_train_features, X_test_features, Y_train, Y_test, feature_extraction = loader.load_data()
 print(Y_test)
 model = SVC(kernel='sigmoid', C=1.0)
 model.fit(X_train_features, Y_train)
