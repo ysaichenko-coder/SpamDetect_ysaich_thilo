4972e9380866afe360788c41d898a3509ce68438
Itiar <timothyhilonenko@gmail.com>
2026-01-19 19:46:18 +0100

    Refactoring

--- data_load.py
+++ data_load.py
@@ -3,17 +3,28 @@ import os
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
 class DataLoad:
+    # handles data loading and preprocessing.
+    
+    def __init__(self, data_path='./data', csv_filename='spam.csv'):
+        self.data_path = data_path
+        self.csv_filename = csv_filename
+        self.feature_extractor = None
+    
     def load_data(self):
-        path = './data'
-        csv_path = os.path.join(path, 'spam.csv')
+        # load and preprocess data."""
+        csv_path = os.path.join(self.data_path, self.csv_filename)
         data = pd.read_csv(csv_path, encoding='latin-1')
         data = data[['v1', 'v2']]
         print(data['v1'].value_counts())
         print(f"total rows: {len(data)}")
-        X_train, X_test, Y_train, Y_test = train_test_split(data['v2'], data['v1'], test_size=0.2, random_state=3)
-        feature_extraction = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
-        X_train_features = feature_extraction.fit_transform(X_train)
-        X_test_features = feature_extraction.transform(X_test)
-        return X_train_features, X_test_features, Y_train, Y_test, feature_extraction
+        
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            data['v2'], data['v1'], test_size=0.2, random_state=3
+        )
+        
+        self.feature_extractor = TfidfVectorizer(min_df=1, stop_words='english', lowercase=True)
+        X_train_features = self.feature_extractor.fit_transform(X_train)
+        X_test_features = self.feature_extractor.transform(X_test)
+        
+        return X_train_features, X_test_features, Y_train, Y_test
--- main.py
+++ main.py
@@ -1,28 +1,64 @@
-import os
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 import joblib
 from data_load import DataLoad
 
-path = './data'
-csv_path = os.path.join(path, 'spam.csv')
-loader = DataLoad()
-X_train_features, X_test_features, Y_train, Y_test, feature_extraction = loader.load_data()
-print(Y_test)
-modelSVC = SVC(kernel='sigmoid', C=1.0)
-modelLR = LogisticRegression()
-for i in [modelSVC, modelLR]:
-    i.fit(X_train_features, Y_train)
-    prediction = i.predict(X_test_features)
-    accuracy = accuracy_score(Y_test, prediction)
-    print(i, 'accuracy on test', accuracy * 100, '%')
-joblib.dump(modelSVC, 'model.joblib')
-input = ["Hello mom, are we having dinner tonight?",
-         "URGENT! You have won a FREE iPhone. Click here",
-         "Call me back later",
-         "Congratulations, you won 1000$ prize money",
-         "Hey bro what's up"]
-input_features = feature_extraction.transform(input)
-prediction = modelSVC.predict(input_features)
-print(prediction)
+
+class ModelTrainer:
+    # handles model training and evaluation.
+
+    def __init__(self, X_train, X_test, y_train, y_test, feature_extractor):
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
+        self.feature_extractor = feature_extractor
+
+    def train_and_evaluate(self, model):
+        # train model and return accuracy.
+        model.fit(self.X_train, self.y_train)
+        prediction = model.predict(self.X_test)
+        accuracy = accuracy_score(self.y_test, prediction)
+        return accuracy
+
+    def predict(self, model, texts):
+        # predict spam/ham for text messages.
+        features = self.feature_extractor.transform(texts)
+        return model.predict(features)
+
+    def save_model(self, model, filepath='model.joblib'):
+        # save trained model.
+        joblib.dump(model, filepath)
+
+
+if __name__ == '__main__':
+    # load data
+    loader = DataLoad()
+    X_train_features, X_test_features, Y_train, Y_test = loader.load_data()
+
+    # train and evaluate models
+    trainer = ModelTrainer(
+        X_train_features, X_test_features, Y_train, Y_test, loader.feature_extractor
+    )
+
+    modelSVC = SVC(kernel='sigmoid', C=1.0)
+    modelLR = LogisticRegression()
+
+    for model in [modelSVC, modelLR]:
+        accuracy = trainer.train_and_evaluate(model)
+        print(f'{model.__class__.__name__} accuracy on test: {accuracy * 100:.2f}%')
+
+    # save best model
+    trainer.save_model(modelSVC, 'model.joblib')
+
+    # test predictions
+    test_messages = [
+        "Hello mom, are we having dinner tonight?",
+        "URGENT! You have won a FREE iPhone. Click here",
+        "Call me back later",
+        "Congratulations, you won 1000$ prize money",
+        "Hey bro what's up"
+    ]
+    predictions = trainer.predict(modelSVC, test_messages)
+    print(predictions)
