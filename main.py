8d7c425a95ce6b520042229b6e4bcc7b03bd820f
YaroslavSaichenko <ysaichenko@edu.cdv.pl>
2026-01-19 15:10:34 +0100

    finally, programm can read our messages from input,check accuracy and which message spam or not.now we have accuracy 96.23%

--- main.py
+++ main.py
@@ -10,14 +10,22 @@ path = './data'
 csv_path = os.path.join(path, 'spam.csv')
 data = pd.read_csv(csv_path,encoding='latin-1')
 data = data[['v1', 'v2']]
-data.loc[data['v1'] == 'spam', 'v1',] = 0
-data.loc[data['v1'] == 'ham', 'v1',] = 1
 X_train, X_test, Y_train, Y_test = train_test_split(data['v2'],data['v1'], test_size=0.2, random_state=3)
 feature_extraction = TfidfVectorizer(min_df=1, stop_words='english',lowercase=True)
 X_train_features = feature_extraction.fit_transform(X_train)
 X_test_features = feature_extraction.transform(X_test)
-
-Y_train = Y_train.astype('int')
-Y_test = Y_test.astype('int')
 print(data['v1'])
 print(Y_test)
+model = LogisticRegression()
+model.fit(X_train_features, Y_train)
+prediction = model.predict(X_test_features)
+accuracy = accuracy_score(Y_test, prediction)
+print('accuracy on test',accuracy*100,'%')
+input = ["Hello mom, are we having dinner tonight?", 
+    "URGENT! You have won a FREE iPhone. Click here", 
+    "Call me back later",                             
+    "Congratulations, you won 1000$ prize money",     
+    "Hey bro what's up"]
+input_features = feature_extraction.transform(input)
+prediction = model.predict(input_features)
+print(prediction)
